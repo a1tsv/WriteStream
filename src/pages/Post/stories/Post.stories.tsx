da2c89d33700b9cb1f4186d45fcbd959b0d4ad8b
a1tsv <altsvbox@gmail.com>
2023-02-06 08:33:13 -0900

    fix: story for post page

--- src/pages/Post/stories/Post.stories.tsx
+++ src/pages/Post/stories/Post.stories.tsx
@@ -3,6 +3,7 @@ import { PostPage } from '@pages/Post'
 import { baseURL } from '@shared/utils/baseURL'
 import { ComponentMeta } from '@storybook/react'
 import { rest } from 'msw'
+import { useEffect } from 'react'
 import { MemoryRouter, Routes } from 'react-router'
 import { Route } from 'react-router-dom'
 
@@ -10,25 +11,33 @@ export default {
 	title: 'Post',
 	component: PostPage,
 	decorators: [StoreDecorator],
-	msw: [
-		rest.get(`${baseURL}/posts/:id`, (req, res, ctx) => {
-			return res(
-				ctx.json({
-					id: '123',
-					blogName: 'My Blog',
-					title: 'My Post',
-					content: 'This is my post content',
-					createdAt: '2023-02-06T00:00:00.000Z'
-				})
-			)
-		})
-	]
+	parameters: {
+		msw: [
+			rest.get(`${baseURL}/posts/1`, (req, res, ctx) => {
+				return res(
+					ctx.json({
+						id: '123',
+						blogName: 'My Blog',
+						title: 'My Post',
+						content: 'This is my post content',
+						createdAt: '2023-02-06T00:00:00.000Z'
+					})
+				)
+			})
+		]
+	}
 } as ComponentMeta<typeof PostPage>
 
-export const Default = () => (
-	<MemoryRouter initialEntries={['posts/1']}>
-		<Routes>
-			<Route path={'/posts/:id'} element={<PostPage />} />
-		</Routes>
-	</MemoryRouter>
-)
+export const Default = () => {
+	useEffect(() => {
+		document.body.dataset.theme = 'light'
+	}, [])
+
+	return (
+		<MemoryRouter initialEntries={['/posts/1']}>
+			<Routes>
+				<Route path={'/posts/:id'} element={<PostPage />} />
+			</Routes>
+		</MemoryRouter>
+	)
+}
