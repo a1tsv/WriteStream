7b91b1beb7897c822ef71481909fc798936500f3
a1tsv <altsvbox@gmail.com>
2023-02-08 07:14:20 -0900

    feat: added new tests for post.api

--- src/entities/Post/api/postApi.test.tsx
+++ src/entities/Post/api/postApi.test.tsx
@@ -29,15 +29,47 @@ describe('post api', () => {
 		expect(method).toBe('GET')
 	})
 
-	// it('should return a post on successful GET request', async () => {
-	// 	const post = { id: '1', title: 'Post 1', body: 'This is post 1' }
-	//
-	// 	fetchMock.mockResponseOnce(JSON.stringify({ data: post }))
-	// 	expect(fetchMock).toHaveBeenCalledTimes(1)
-	// 	await store.dispatch(api.endpoints.getPost.initiate('1'))
-	// 	const calls = fetchMock.mock.calls[0][0] as Request
-	// 	const { url, method } = calls
-	// 	expect(url).toBe(`${baseURL}/posts/1`)
-	// 	expect(method).toBe('GET')
-	// })
+	it('gets a specific post', async () => {
+		const post = { id: '1', title: 'Post 1', content: 'This is post 1' }
+		fetchMock.mockResponseOnce(JSON.stringify({ data: post }))
+		await store.dispatch(api.endpoints.getPost.initiate('1'))
+		expect(fetchMock).toHaveBeenCalledTimes(1)
+		const calls = fetchMock.mock.calls[0][0] as Request
+		const { url, method } = calls
+		expect(url).toBe(`${baseURL}/posts/1`)
+		expect(method).toBe('GET')
+	})
+
+	it('creates a post', async () => {
+		const post = { id: '1', title: 'Post 1', content: 'This is post 1' }
+		fetchMock.mockResponseOnce(JSON.stringify({ data: post }))
+		await store.dispatch(api.endpoints.createPost.initiate(post))
+		expect(fetchMock).toHaveBeenCalledTimes(1)
+		const calls = fetchMock.mock.calls[0][0] as Request
+		const { url, method } = calls
+		expect(url).toBe(`${baseURL}/posts`)
+		expect(method).toBe('POST')
+	})
+
+	it('updates a post', async () => {
+		const post = { id: '1', title: 'Post 1', content: 'This is post 1' }
+		fetchMock.mockResponseOnce(JSON.stringify({ data: post }))
+		await store.dispatch(api.endpoints.updatePost.initiate(post))
+		expect(fetchMock).toHaveBeenCalledTimes(1)
+		const calls = fetchMock.mock.calls[0][0] as Request
+		const { url, method } = calls
+		expect(url).toBe(`${baseURL}/posts/1`)
+		expect(method).toBe('PUT')
+	})
+
+	it('deletes a post', async () => {
+		const post = { id: '1', title: 'Post 1', content: 'This is post 1' }
+		fetchMock.mockResponseOnce(JSON.stringify({ data: post }))
+		await store.dispatch(api.endpoints.deletePost.initiate('1'))
+		expect(fetchMock).toHaveBeenCalledTimes(1)
+		const calls = fetchMock.mock.calls[0][0] as Request
+		const { url, method } = calls
+		expect(url).toBe(`${baseURL}/posts/1`)
+		expect(method).toBe('DELETE')
+	})
 })
